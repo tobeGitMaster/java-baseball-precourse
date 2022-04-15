57f88db90913d88d251471054e86d35f6b1c33df
pauljung <cse2012@naver.com>
2022-04-15 23:02:54 +0900

    Test: 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.model.GameResult;
+import baseball.service.GameService;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -32,4 +41,56 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    void 서로다른3개숫자_생성확인_테스트() {
+        // given
+        GameService gameService = new GameService();
+        List<Integer> numbers = gameService.getRandomNumbers();
+
+        // when
+        // 숫자 중복 제거
+        Set<Integer> set = new HashSet<>(numbers);
+
+        // then
+        assertThat(set.size()).isEqualTo(numbers.size());
+    }
+
+
+    @Test
+    void _3볼테스트() {
+        // given
+        GameService gameService = new GameService();
+        List<Integer> numbers = gameService.getRandomNumbers();
+
+        // 순서 바꾸기
+        List<Integer> numbers_copy = new ArrayList<>(numbers);
+        numbers_copy.add(0, numbers_copy.remove(2));
+
+        String input = "";
+        for (int i : numbers_copy) input += i;
+
+        // when
+        GameResult gameResult = gameService.process(input);
+
+        // then
+        Assertions.assertThat("3볼").isEqualTo(gameResult.getMessage());
+    }
+
+    @Test
+    void 특수문자_입력예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12#"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void _0입력예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 }
